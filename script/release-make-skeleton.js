cf1e1e307eab4df8b51c9005883e573376dad929
Thomas Deconinck <thomas.deconinck@colisweb.com>
2021-04-14 10:00:34 +0200

    chore: update deploy tempalte

--- script/release-make-skeleton.js
+++ script/release-make-skeleton.js
@@ -1,56 +1,59 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process')
-const fs = require('fs')
-const path = require('path')
-const esy = require('../esy.json')
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const esy = require("../esy.json");
 
-console.log(`Using esy.json:\n${esy}`)
+console.log(`Using esy.json:\n${esy}`);
 
-const filesToCopy = ['LICENSE', 'README.md']
+const filesToCopy = ["LICENSE", "README.md"];
 
 function exec(cmd) {
-  console.log(`exec: ${cmd}`)
-  return execSync(cmd).toString()
+  console.log(`exec: ${cmd}`);
+  return execSync(cmd).toString();
 }
 
 function mkdirpSync(p) {
   if (fs.existsSync(p)) {
-    return
+    return;
   }
-  mkdirpSync(path.dirname(p))
-  fs.mkdirSync(p)
+  mkdirpSync(path.dirname(p));
+  fs.mkdirSync(p);
 }
 
 function removeSync(p) {
-  exec(`rm -rf -i "${p}"`)
+  exec(`rm -rf -i "${p}"`);
 }
 
-const src = path.resolve(path.join(__dirname, '..'))
-const dst = path.resolve(path.join(__dirname, '..', '_release'))
+const src = path.resolve(path.join(__dirname, ".."));
+const dst = path.resolve(path.join(__dirname, "..", "_release"));
 
-mkdirpSync(dst)
-removeSync(dst)
-mkdirpSync(dst)
+mkdirpSync(dst);
+removeSync(dst);
+mkdirpSync(dst);
 
 for (const file of filesToCopy) {
-  const p = path.join(dst, file)
-  mkdirpSync(path.dirname(p))
-  fs.copyFileSync(path.join(src, file), p)
+  const p = path.join(dst, file);
+  mkdirpSync(path.dirname(p));
+  fs.copyFileSync(path.join(src, file), p);
 }
 
-fs.copyFileSync(path.join(src, 'script', 'release-postinstall.js'), path.join(dst, 'postinstall.js'))
+fs.copyFileSync(
+  path.join(src, "script", "release-postinstall.js"),
+  path.join(dst, "postinstall.js")
+);
 
-const filesToTouch = ['ppx.exe', 'ppx', 'extract.exe', 'extract']
+const filesToTouch = ["ppx.exe", "ppx", "extract.exe", "extract"];
 
 for (const file of filesToTouch) {
-  const p = path.join(dst, file)
-  mkdirpSync(path.dirname(p))
-  fs.writeFileSync(p, '')
+  const p = path.join(dst, file);
+  mkdirpSync(path.dirname(p));
+  fs.writeFileSync(p, "");
 }
 
 const pkgJson = {
-  name: 'res-react-intl',
+  name: "@dck/res-react-intl",
   version: esy.version,
   description: esy.description,
   homepage: esy.homepage,
@@ -58,24 +61,27 @@ const pkgJson = {
   repository: esy.repository,
   keywords: esy.keywords,
   scripts: {
-    postinstall: 'node postinstall.js',
+    postinstall: "node postinstall.js",
   },
   bin: {
-    'res-react-intl-ppx.exe': 'ppx.exe',
-    'res-react-intl-extract.exe': 'extract.exe',
-    'res-react-intl-ppx': 'ppx',
-    'res-react-intl-extract': 'extract',
+    "res-react-intl-ppx.exe": "ppx.exe",
+    "res-react-intl-extract.exe": "extract.exe",
+    "res-react-intl-ppx": "ppx",
+    "res-react-intl-extract": "extract",
   },
   files: [
-    'platform-windows-x64/',
-    'platform-linux-x64/',
-    'platform-darwin-x64/',
-    'postinstall.js',
-    'ppx.exe',
-    'ppx',
-    'extract.exe',
-    'extract',
+    "platform-windows-x64/",
+    "platform-linux-x64/",
+    "platform-darwin-x64/",
+    "postinstall.js",
+    "ppx.exe",
+    "ppx",
+    "extract.exe",
+    "extract",
   ],
-}
+};
 
-fs.writeFileSync(path.join(dst, 'package.json'), JSON.stringify(pkgJson, null, 2))
+fs.writeFileSync(
+  path.join(dst, "package.json"),
+  JSON.stringify(pkgJson, null, 2)
+);
